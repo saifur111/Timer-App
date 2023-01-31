fc62389c69afdac3709a332bf672bd024ef41a25
saifur111 <ssaaiiffuurr240@gmail.com>
2023-01-31 10:31:56 +0600

    Components & containers updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Input from './containers/Input';
-import View from './containers/View';
+import logo from "./logo.svg";
+import "./App.css";
+import Input from "./containers/Input";
+import View from "./containers/View";
 
 function App() {
   return (
     <div className="App">
-      <div className="Mid">
-          <h1>COUNTDOWN TIMER</h1>
-          <Input />
-          <View />
-        </div>
+      <div className="first">
+        <h1>COUNTDOWN TIMER</h1>
+        <Input />
+        <View />
+        <br />
+        <span><a href="https://github.com/saifur111/Timer-App">Please Visit Timer-App Source Code</a></span>
+      </div>
     </div>
   );
 }
